59504d4b607e98921e902eaa4e6db3c10cee585b
Ikey Doherty <ikey@solus-project.com>
2016-10-13 22:25:11 +0100

    Move to setuptools (using distutils) for installation
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name            = "doflicky",
+    version         = "4",
+    author          = "Ikey Doherty",
+    author_email    = "ikey@solus-project.com",
+    description     = ("Solus Driver Manager"),
+    license         = "GPL-2.0",
+    url             = "https://github.com/solus-project/doflicky",
+    packages        = ['doflicky'],
+    scripts         = ['doflicky-ui'],
+    classifiers     = [ "License :: OSI Approved :: GPL-2.0 License"],
+    data_files      = [("/usr/share/applications", ["doflicky.desktop"]),
+                       ("/usr/share/doflicky/modaliases", ["aliases/broadcom-sta.modaliases"])]
+)
