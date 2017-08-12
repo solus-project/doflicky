049e280a2dfe3b9a7ae5c3e635a03ef71f5b389b
Ikey Doherty <ikey@solus-project.com>
2017-08-12 15:19:47 +0100

    Fix copyright data range assignments
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- doflicky/widgets.py
+++ doflicky/widgets.py
@@ -3,7 +3,7 @@
 #
 #  widgets.py - SSC Helper Widgets
 #
-#  Copyright 2014 Ikey Doherty <ikey.doherty@gmail.com>
+#  Copyright 2014-2017 Ikey Doherty <ikey.doherty@gmail.com>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
