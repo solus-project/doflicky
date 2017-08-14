8c81cd906ba55e2412b10ed0d5d8ebb23c0542a9
Ikey Doherty <ikey@solus-project.com>
2017-08-14 19:47:01 +0100

    Release v6 for multikernel
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name            = "doflicky",
-    version         = "5",
+    version         = "6",
     author          = "Ikey Doherty",
     author_email    = "ikey@solus-project.com",
     description     = ("Solus Driver Manager"),
