d798c04624f298230d3c3682e0f4f9302b0f23cf
Ikey Doherty <ikey@solus-project.com>
2017-08-14 18:15:14 +0100

    Add support for broadcom-sta detection
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ doflicky/driver/broadcom.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#  This file is part of doflicky
+#
+#  Copyright © 2017 Ikey Doherty <ikey@solus-project.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+
+from doflicky.detection import DriverBundlePCI
+
+
+class DriverBundleBroadcom(DriverBundlePCI):
+    """ Main NVIDIA driver (nvidia-glx-driver) """
+
+    def __init__(self):
+        DriverBundlePCI.__init__(self, "broadcom-sta.modaliases")
+
+    def get_name(self):
+        return "Broadcom's IEEE 802.11a/b/g/n hybrid Linux device driver"
+
+    def get_icon(self):
+        return "network-server"
+
+    def has_emul32(self):
+        return False
+
+    def get_base(self):
+        return "broadcom-sta"
+
+    def get_priority(self):
+        return 1
+
+    def get_packages(self, emul32=False):
+        # TODO: Return the right packages for the kernel!"
+        return ["broadcom-sta"]
